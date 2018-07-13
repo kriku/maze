141378b39b0c070b17037e22b080e511399ce12c
Gosha Krikun <g.krikun@innopolis.ru>
2018-07-13 12:09:34 +0300

    Add animation

--- src/robot.js
+++ src/robot.js
@@ -8,16 +8,52 @@ import Playbot_run from 'engine/examples/assets/Playbot/Playbot_run.json';
 
 export default class Robot {
     constructor(app) {
-        const bot = app.loader.open('model', Playbot, 'pseudo.json');
+        const botModel = app.loader.open('model', Playbot, '.json');
+
+        const idleAnimation = app.loader.open('animation', Playbot_idle, '.json');
+        idleAnimation.setName('idle');
+        const runAnimation = app.loader.open('animation', Playbot_run, '.json');
+        runAnimation.setName('run');
 
         const entity = new pc.Entity();
-        entity.addComponent("model");
-        entity.model.model = bot;
+        entity.setName('BadRobot');
+
+        entity.addComponent('model');
+        entity.model.model = botModel;
+
+        entity.addComponent('animation');
+        entity.animation.animations['idle'] = idleAnimation;
+        entity.animation.animations['run'] = runAnimation;
+        entity.animation.setModel(botModel);
+
+        setTimeout(() => {
+            this.run();
+        }, 1000);
+
         entity.setLocalPosition(0, 0, 2);
 
         var e = entity.getLocalEulerAngles();
         entity.setLocalEulerAngles(e.x + 90, e.y, e.z);
 
         app.root.addChild(entity);
+        this.entity = entity;
+        // punch from console
+        window.badRobot = entity;
+    }
+
+    // Start running then stop in 1s
+    run() {
+        this.entity.animation.play("run", 0.2);
+        setTimeout(() => {
+            this.stop();
+        }, 1000);
+    };
+
+    // Stop running then start running in 1s
+    stop() {
+        this.entity.animation.play("idle", 0.2);
+        setTimeout(() => {
+            this.run();
+        }, 1000);
     }
 }
